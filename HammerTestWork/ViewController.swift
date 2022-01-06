c1c3c4d6890f07b34fc635bc38f1e0286f996abb
SavvaShuliatev <71968903+SavvaShuliatev@users.noreply.github.com>
2022-01-06 13:08:02 +0300

    Initial Commit

---
+++ HammerTestWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HammerTestWork
+//
+//  Created by Савва Шулятьев on 06.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
